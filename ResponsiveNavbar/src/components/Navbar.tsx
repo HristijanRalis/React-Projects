ccaf5fa90df309f086ed4cc9d0f3d2879750f79c
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-10-06 12:35:29 +0200

    Done with hamburger menu

--- ResponsiveNavbar/src/components/Navbar.tsx
+++ ResponsiveNavbar/src/components/Navbar.tsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
   return (
     <div className="Navbar d-flex justify-content-between">
       <div className="LOGO">
         <h5>LOGO...</h5>
       </div>
 
-      <ul className="NavList d-flex justify-content-between gap-5 ">
+      <ul className="NavList  gap-5 ">
         <li>
           <Link to="/" className="list-item">
             Home
@@ -27,27 +31,29 @@ export const Navbar = () => {
         </li>
       </ul>
 
-      <div className="hamburger">
+      <div className="hamburger" onClick={toggleMenu}>
         <FontAwesomeIcon icon={faBars} size="lg" className="hamburgerIcon" />
       </div>
 
-      <ul className="hamburgerList">
-        <li>
-          <Link to="/" className="list-item">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/content" className="list-item">
-            Content
-          </Link>
-        </li>
-        <li>
-          <Link to="/aboutUs" className="list-item">
-            About Us
-          </Link>
-        </li>
-      </ul>
+      {menuOpen && (
+        <ul className="hamburgerList">
+          <li>
+            <Link to="/" className="hamburger-list-item">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/content" className="hamburger-list-item">
+              Content
+            </Link>
+          </li>
+          <li>
+            <Link to="/aboutUs" className="hamburger-list-item">
+              About Us
+            </Link>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
